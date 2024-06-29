5d329b5b0106e02626e77eac298f37cc043b73b2
qiffym <qiww.dev@gmail.com>
2024-06-29 16:32:28 +0700

    layouting & showing data homepage

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ isset($title) ? $title : config('app.name') }}</title>
+
+    <link href="{{ asset('css/output.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+    <!-- CSS for carousel/flickity-->
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/flickity-fade@2/flickity-fade.css">
+
+    <!-- CSS for modal/flowbite -->
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" /> -->
+</head>
+
+<body>
+    {{ $slot }}
+
+    @isset($after_script)
+        {{ $after_script }}
+    @endisset
+</body>
+
+</html>
